a47990dba004592b90f73e17231ac9fa2e11f5ed
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-18 14:30:49 -0500

    Updated from Brython Server: 12/18/2017 2:30:49 PM

--- dotsAndBoxes.py
+++ dotsAndBoxes.py
@@ -72,7 +72,8 @@ if __name__ == '__main__': #This isn't really a function, but the rest of your c
     redrawAll()
     buildBoard()
     
-    App().run(mouseClick)
+    App().listenMouseEvent("click", mouseClick)
+    App().run(event)
 
 '''
 Useful detail - You will probably want to look at the Matrix Demo to remind yourself how to create and work with a matrix.
