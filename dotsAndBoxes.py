7b5091fa4188d9fa2ee8afcbf40b9f9778810a30
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-18 14:44:09 -0500

    Updated from Brython Server: 12/18/2017 2:44:09 PM

--- dotsAndBoxes.py
+++ dotsAndBoxes.py
@@ -50,6 +50,8 @@ def updateBottomEdge(): #See updateLeftEdge
 '''
 def mouseClick(event): #Should take one argument, event. The function should figure out where the user clicked (event.x and event.y have the coordinates of the click). The function should figure out which row and column the user clicked and if it is closest to the top edge, bottom edge, left edge, or right edge of the square. The appropriate edge should then be updated.
     print(event.x,event.y)
+    if 0<event.x<10 and 0<event.y<100:
+        print('Bingo')
 
 if __name__ == '__main__':
     
