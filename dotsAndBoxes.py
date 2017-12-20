a067ff3762dfe9426cb2b87218c3e722c43cb7c0
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-19 22:10:52 -0500

    Updated from Brython Server: 12/19/2017 10:10:52 PM

--- dotsAndBoxes.py
+++ dotsAndBoxes.py
@@ -27,10 +27,14 @@ def redrawAll(): #Should take no arguments. Deletes all the graphics on the boar
         for col in range(0,4):
             Sprite(horRectangle, (110*col+10, 110*row))
     '''
+    
+    drawLeftEdge()
 
 
 def drawLeftEdge(row,col,x,y): #Should take four (or six) arguments, the row and column numbers of the cell that you are working on and the x and y coordinate of the upper lefthand corner. You can also include the coordinates of the bottom righthand corner if you want. This function should draw the edge of the square in the appropriate color.
-    print('hi')
+    for col in range(0,4):
+        for row in range(0,4):
+            Sprite(Sprite(verRectangle, (110*col, 110*row+10)))
 
 '''
 def drawRightEdge(row,col,x,y): #See drawLeftEdge
