79c14e49971e18301aa8237866a99313e8bb4593
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-01-04 14:33:59 -0500

    Updated from Brython Server: 1/4/2018 2:33:59 PM

--- dotsAndBoxes.py
+++ dotsAndBoxes.py
@@ -76,10 +76,10 @@ def drawCenters(): #See drawLeftEdge. The function should color in the center an
         for col in range(0,4):
             if data['matrix'][col][row][4]==1:
                 Sprite(one, (110*col+10, 110*row))
-                #DO COLOR
+                Sprite(redSq, (110*col+10, 110*row))
             if data['matrix'][col][row][4]==2:
                 Sprite(two, (110*col+10, 110*row+10))
-                #DO COLOR
+                Sprite(blueSq, (110*col+10, 110*row))
 
 '''
 def drawScore(): #Should take no arguments. The function print the current score as well as detect if the game is over.
