e880548d0a2c15ae445b1297c65fae2b22000d46
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-01-04 21:44:47 -0500

    Updated from Brython Server: 1/4/2018 9:44:47 PM

--- dotsAndBoxes.py
+++ dotsAndBoxes.py
@@ -79,7 +79,6 @@ def drawCenters(): #See drawLeftEdge. The function should color in the center an
                 Sprite(two, (110*col+50, 110*row+25))
 
 def drawScore(): #Should take no arguments. The function print the current score as well as detect if the game is over.
-    print(data['score1'],data['score2'])
     Sprite(TextAsset(data['score1'], fill=red, style='bold 30pt Times'))
     Sprite(TextAsset(data['score2'], fill=blue, style='bold 30pt Times'),(50,0))
     
