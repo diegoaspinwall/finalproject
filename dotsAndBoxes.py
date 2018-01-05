d252e464f53a3e136d9097a66c64ed0083f7e7c5
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-01-04 19:03:48 -0500

    Updated from Brython Server: 1/4/2018 7:03:48 PM

--- dotsAndBoxes.py
+++ dotsAndBoxes.py
@@ -119,10 +119,10 @@ def updateBottomEdge(row,col): #See updateLeftEdge
             data['matrix'][col][row][3]=1
             if data['matrix'][col][row][0] != 0 and data['matrix'][col][row][1] != 0 and data['matrix'][col][row][2] != 0:
                 data['matrix'][col][row][4] = 1
-                data['score1'] += 1
+                '''data['score1'] += 1
                 data['scoreTextRed'].destroy()
                 scoreRed = TextAsset(str(data['score1']),fill=red, style='bold 30pt Times')
-                data['scoreTextRed'] = Sprite(scoreRed)
+                data['scoreTextRed'] = Sprite(scoreRed)'''
         if data['player'] == -1:
             data['matrix'][col][row][3]=2
             if data['matrix'][col][row][0] != 0 and data['matrix'][col][row][1] != 0 and data['matrix'][col][row][2] != 0:
@@ -215,7 +215,7 @@ if __name__ == '__main__':
     redrawAll()
     
     data['scoreTextRed'] = Sprite(scoreRed)
-    data['scoreTextBlue'] = Sprite(scoreBlue)
+    data['scoreTextBlue'] = Sprite(scoreBlue,(50,0))
     
     App().listenMouseEvent("click", mouseClick)
     App().run()
