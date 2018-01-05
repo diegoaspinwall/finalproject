81211bcf68ed17767f725fef85dffc69520fe5d6
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-01-04 21:40:27 -0500

    Updated from Brython Server: 1/4/2018 9:40:28 PM

--- dotsAndBoxes.py
+++ dotsAndBoxes.py
@@ -79,6 +79,9 @@ def drawCenters(): #See drawLeftEdge. The function should color in the center an
 
 def drawScore(): #Should take no arguments. The function print the current score as well as detect if the game is over.
     print(data['score1'],data['score2'])
+    Sprite(scoreRed)
+    Sprite(scoreBlue,(50,0))
+    
 
 def updateLeftEdge(row,col): #Should take two arguments, the row and column number of the square that was just clicked. The function should update the matrix for that column to indicate which player clicked the left edge of that box. The function should also update the right edge of the neighboring box if there is one.
     if data['matrix'][col][row][0] == 0:
